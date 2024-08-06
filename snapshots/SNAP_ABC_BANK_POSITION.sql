f6e94e4fef878e939eb4eb1a8c27937a4dc2be32
Gregory Tomy <gregory.tomy@colorado.edu>
2024-08-06 23:45:40 +0000

    added snapshot history by changing stg model to ephemeral, updated ref model to pull active positions

---
+++ snapshots/SNAP_ABC_BANK_POSITION.sql
@@ -0,0 +1,21 @@
+{% snapshot SNAP_ABC_BANK_POSITION %}
+
+    /*
+We have used the check strategy as we do not trust the REPORT_DATE column
+to work as a good timestamp for the timestamp strategy.
+
+We have also enabled tracking of deletions from the source.  
+*/
+    {{
+        config(
+            unique_key="position_hkey",
+            strategy="check",
+            check_cols=["position_hdiff"],
+            invalidate_hard_deletes=True,
+        )
+    }}
+
+    select *
+    from {{ ref("STG_ABC_BANK_POSITION") }}
+
+{% endsnapshot %}
