3a05c6aa13e216298259271eaa9369b4cc1c01c8
Gregory Tomy <gregory.tomy@colorado.edu>
2024-08-07 14:54:57 +0000

    staged position dimension, added default records to position, created snapshot and saved history

---
+++ models/staging/STG_ABC_BANK_SECURITY_INFO.sql
@@ -0,0 +1,53 @@
+{{ config(materialized="ephemeral") }}
+
+with
+    source_data as (
+        select
+            security_code as security_code,  -- TEXT
+            security_name as security_name,  -- TEXT
+            sector as sector_name,  -- TEXT
+            industry as industry_name,  -- TEXT
+            country as country_code,  -- TEXT
+            exchange as exchange_code,  -- TEXT
+            load_ts as load_ts,  -- TIMESTAMP_NTZ
+            'seed.ABC_Bank_SECURITY_INFO' as record_source
+
+        from {{ source("seeds", "ABC_Bank_SECURITY_INFO") }}
+    ),
+
+    default_record as (
+        select 
+            '-1' as security_code,
+            'Missing' as security_name,
+            'Missing' as sector_name,
+            'Missing' as industry_name,
+            '-1' as country_code,
+            '-1' as exchange_code,
+            '2020-01-01' as load_ts_utc,
+            'System.DefaultKey' as record_source
+    ),
+
+    with_default_records as (
+        select * from source_data
+        union
+        select * from default_record
+    ),
+
+    hashed as (
+        select
+            concat_ws('|', security_code) as security_hkey,
+            concat_ws(
+                '|',
+                security_code,
+                security_name,
+                sector_name,
+                industry_name,
+                country_code,
+                exchange_code
+            ) as security_hdiff,
+            * exclude load_ts,
+            load_ts as load_ts_utc  -- renaming it in the hashed column
+        from source_data
+    )
+
+    select * from hashed
